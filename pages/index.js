7a6ee525336f15c690428eef0cc36e0cd2604a50
Muhanned Noman <Muhanned.Noman@gmail.com>
2021-06-21 05:59:59 +0300

    Feat: redirect if logged in
    This will redirect the user automatically to `/dashboard`
    if they are logged in.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
+import Head from 'next/head';
 import { Flex, Button } from '@chakra-ui/react';
 
 import { useAuth } from '@/lib/auth';
 import Logo from '@/icons/Logo';
-import EmptyPlan from '@/components/EmptyPlan';
 
 export default function Home() {
   const auth = useAuth();
@@ -14,14 +14,36 @@ export default function Home() {
       align="center"
       justify="center"
       h="100vh"
-      >
+      maxW="400px"
+      margin="0 auto"
+    >
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              if (document.cookie && document.cookie.includes('saas-feedback-auth')) {
+                window.location.href = "/dashboard"
+              }
+            `
+          }}
+        />
+        <title>Fast Feedback</title>
+      </Head>
       <Logo />
       {auth.user ? (
-          <EmptyPlan />
-          // <Button onClick={() => auth.signout()}>Sign Out</Button>
-        ) : (
-          <Button mt={4} size="sm" onClick={() => auth.signinWithGithub()}>Sign In</Button>
-        )}
-      </Flex>
+        <Button as="a" size="sm" fontWeight="medium" href="/dashboard">
+          View Dashboard
+        </Button>
+      ) : (
+        <Button
+          mt={4}
+          size="sm"
+          fontWeight="medium"
+          onClick={() => auth.signinWithGithub()}
+        >
+          Sign In
+        </Button>
+      )}
+    </Flex>
   );
 }
